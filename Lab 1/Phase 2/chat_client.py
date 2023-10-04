e1d6d866864ce2444da7b1ee8cd2c8049d3e1993
SuperGawp <aryanchandra2002@hotmail.com>
2023-10-03 19:35:25 -0600

    fixed phase 2

--- Lab 1/Phase 2/chat_client.py
+++ Lab 1/Phase 2/chat_client.py
@@ -1,4 +1,5 @@
 import socket
+import threading 
 
 PORT = 8000
 PC_NAME = socket.gethostname()
@@ -13,22 +14,41 @@ def send (sock, message):
     message_length = len(message_encoded)
     message_send_length = str(message_length).encode(FORMAT)
     padding_needed = HEADER_SIZE_IN_BYTES - len(message_send_length)
-    # this repeats the space byte by
+    # this repeats the space byte by padding needed times
     padding = b' ' * padding_needed
     padded_message_send_length = message_send_length + padding
     sock.send(padded_message_send_length)
     sock.send(message_encoded)
     
+def receive_messages(sock):
+    while True:
+        try:
+            message_length_encoded = sock.recv(HEADER_SIZE_IN_BYTES)
+            message_length_string = message_length_encoded.decode(FORMAT)
+            if message_length_string:
+                message_length_int = int(message_length_string)
+                message_encoded = sock.recv(message_length_int)
+                message = message_encoded.decode(FORMAT)
+                print(f"[{TCP_ADDR}] {message}")
+        except socket.timeout:
+            continue
+
 if __name__ == "__main__":
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(TCP_ADDR)
 
+    # Start a thread to receive messages from the server
+    receive_thread = threading.Thread(target=receive_messages, args=(client,))
+    receive_thread.start()
+
     try:
-        message = input(f"enter message, or enter '{DISCONNECT_MESSAGE}' to diconnect: ")
+        message = input(f"Enter a message, or enter '{DISCONNECT_MESSAGE}' to disconnect: ")
         while message != DISCONNECT_MESSAGE:
-            send (client, message)
+            send(client, message)
             message = input()
     except KeyboardInterrupt:
         pass
-    send (client, DISCONNECT_MESSAGE)
+
+    send(client, DISCONNECT_MESSAGE)
     client.close()
+    receive_thread.join()
--- Lab 1/Phase 2/chat_server.py
+++ Lab 1/Phase 2/chat_server.py
@@ -9,33 +9,36 @@ FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!Q"
 HEADER_SIZE_IN_BYTES = 8
 
-# store the clients here 
 clients = []
-clients_lock = threading.Lock()
 
-# use this to display messages to everyone BUT the sender
-def broadcast(message, sender_addr):
-    with clients_lock:
-        for client, addr in clients:
-            if addr != sender_addr:
-                try:
-                    message_length = len(message)
-                    message_send_length = str(message_length).encode(FORMAT)
-                    padding_needed = HEADER_SIZE_IN_BYTES - len(message_send_length)
-                    padding = b' ' * padding_needed
-                    padded_message_send_length = message_send_length + padding
-                    client.send(padded_message_send_length)
-                    client.send(message.encode(FORMAT))
-                except:
-                    # Handle any errors that may occur while sending to a specific client
-                    print(f"Failed to send message to {addr}")
+def send(client_socket, message):
+    message_encoded = message.encode(FORMAT)
+    message_length = len(message_encoded)
+    message_send_length = str(message_length).encode(FORMAT)
+    padding_needed = HEADER_SIZE_IN_BYTES - len(message_send_length)
+    # This repeats the space byte by padding needed times
+    padding = b' ' * padding_needed
+    padded_message_send_length = message_send_length + padding
+    client_socket.send(padded_message_send_length)
+    client_socket.send(message_encoded)
+
+def broadcast_message(sender_sock, message):
+    for client_sock, addr in clients:
+        if client_sock != sender_sock:
+            try:
+                send(client_sock, f"{str(addr)}: {message}")
+            except:
+                # Handle any exceptions that occur when sending to a client
+                pass
 
 # one instance of this will run for each client in a thread
 def handle_client(conn, addr, server_active):
     print(f"[NEW CONNECTION] {addr} connected.")
-    with clients_lock:
-        clients.append((conn, addr))
     connected = True
+
+    # Add the new client to the clients list
+    clients.append((conn, addr))
+
     while server_active.is_set() and connected:
         try:
             message_length_encoded = conn.recv(HEADER_SIZE_IN_BYTES)
@@ -46,40 +49,46 @@ def handle_client(conn, addr, server_active):
                 message = message_encoded.decode(FORMAT)
                 if message != DISCONNECT_MESSAGE:
                     print(f"[{addr}] {message}")
-                    # Broadcast the message to all clients except the sender
-                    broadcast(f"[{addr}] {message}", addr)
+                    # Broadcast the message to all other clients
+                    broadcast_message(conn, message)
                 else:
                     connected = False
         except socket.timeout:
             continue
-    with clients_lock:
-        clients.remove((conn, addr))
+
+    # Remove the client from the clients list when they disconnect
+    clients.remove((conn, addr))
+
     print(f"[END CONNECTION] {addr} disconnected.")
-    print(f"[ACTIVE CONNECTIONS] {len(clients)}")
+    print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 2}")
     conn.close()
 
 if __name__ == "__main__":
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(TCP_ADDR)
-    print("[STARTING] Server is starting...")
+    print ("[STARTING] Server is starting...")
     server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER_IP}")
+    server.settimeout(1)
+    print (f"[LISTENING] Server is listening on {SERVER_IP}")
     threads = []
     server_active = threading.Event()
     server_active.set()
     try:
         while True:
-            # This is a blocking command, it will wait for a new connection to the server
-            conn, addr = server.accept()
-            conn.settimeout(1)
-            thread = threading.Thread(target=handle_client, args=(conn, addr, server_active))
-            thread.start()
-            threads.append(thread)
-            print(f"[ACTIVE CONNECTIONS] {len(clients)}")
+            try:
+                # this is a blocking command, it will wait for a new connection to the server
+                conn, addr = server.accept()
+                conn.settimeout(1)
+                thread = threading.Thread(target=handle_client, args=(conn, addr, server_active))
+                thread.start()
+                threads.append(thread)
+                print (f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+            except socket.timeout:
+                continue
     except KeyboardInterrupt:
-        print("[SHUTTING DOWN] Attempting to close threads.")
+        print ("[SHUTTING DOWN] Attempting to close threads.")
         server_active.clear()
         for thread in threads:
             thread.join()
-        print(f"[ACTIVE CONNECTIONS] {len(clients)}")
+        print (f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
     server.close()
